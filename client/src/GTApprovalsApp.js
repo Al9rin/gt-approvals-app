29aa985b59022b28a0436fcf8ca4e7c7649479a2
Aldrin <aldrin@Aldrins-MacBook-Air.local>
2025-05-01 15:45:42 -0400

    Update: added new international license data and other fixes

--- client/src/GTApprovalsApp.js
+++ client/src/GTApprovalsApp.js
@@ -25,7 +25,7 @@ const US_STATES = [
   "Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire",
   "New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon",
   "Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont",
-  "Virginia","Washington","West Virginia","Wisconsin","Wyoming","District Columbia",
+  "Virginia","Washington","West Virginia","Wisconsin","Wyoming","District Columbia","Psypact",
 ];
 
 const CANADA_PROVINCES = [
